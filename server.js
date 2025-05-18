fc7dca83bb5fcc3b62f14e6e229b2b5a2536e0fc
Gumer2000 <75317592+Gumer2000@users.noreply.github.com>
2025-05-18 17:36:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,16 @@ server.all('/', (req, res) => {
 });
 
 function keepAlive() {
-  server.listen(3000, () => {
-    console.log('Server sẵn sàng!');
-  });
+  const PORT = process.env.PORT || Math.floor(Math.random() * (65535 - 3001) + 3001);
+  try {
+    server.listen(PORT, () => {
+      console.log(`Server đã sẵn sàng! Port: ${PORT}`);
+    });
+  } catch (err) {
+    console.log('Không thể khởi động server trên port ' + PORT);
+    // Thử port khác nếu port hiện tại bị lỗi
+    keepAlive();
+  }
 }
 
 module.exports = keepAlive; 
